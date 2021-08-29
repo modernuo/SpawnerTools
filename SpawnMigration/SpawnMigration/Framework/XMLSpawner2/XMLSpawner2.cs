5cc4a0374fc50338980fb7d0a087ad326f71d67e
Quick <577652+markdwags@users.noreply.github.com>
2021-08-29 00:21:37 -0500

    Option to define a source and target folder vs individual file (#1)
    * Add .gitignore
    
    * Remove bin, obj, .vs from repo
    
    * Convert a whole folder of XML files
    
    * Remove unused directives, int vs Int32
    
    * Just snag .xml files

--- SpawnMigration/SpawnMigration/Framework/XMLSpawner2/XMLSpawner2.cs
+++ SpawnMigration/SpawnMigration/Framework/XMLSpawner2/XMLSpawner2.cs
@@ -3,122 +3,132 @@ using SpawnMigration.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace SpawnMigration.Framework.XMLSpawner2
 {
     public class XMLSpawner2
     {
-        public XMLSpawner2(string sourcepath, string targetpath, string facet)
+        /// <summary>
+        /// Converts an XML from XMLSpawner2 into a MUO json file
+        /// </summary>
+        /// <param name="sourcePath">A specific .xml file or a path containing multiple</param>
+        /// <param name="targetPath">A specific .json file to save to or a path that all will be saved to</param>
+        /// <param name="facet"></param>
+        public XMLSpawner2(string sourcePath, string targetPath, string facet)
         {
-            using (var fileStream = File.Open(sourcepath, FileMode.Open))
+            var attributes = File.GetAttributes(sourcePath);
+            var files = attributes.HasFlag(FileAttributes.Directory) ? Directory.GetFiles(sourcePath, "*.xml") : new[] { sourcePath };
+            
+            foreach (string currentFile in files)
             {
-
-
-                XmlSerializer serializer = new XmlSerializer(typeof(Root));
-                var parsedObject = (Root)serializer.Deserialize(fileStream);
-
-                List<JSONSpawn> JSONSpawners = new List<JSONSpawn>();
-
-                foreach (var item in parsedObject.Points)
+                using (var fileStream = File.Open(currentFile, FileMode.Open))
                 {
-                    // Save to model list NOTICE: Temp 
-                    List<PointEntry> pointEntries = new List<PointEntry>();
+                    XmlSerializer serializer = new XmlSerializer(typeof(Root));
+                    var parsedObject = (Root)serializer.Deserialize(fileStream);
 
-                    if (item.Objects2 != "" && item.Objects2.Contains("OBJ="))
+                    List<JSONSpawn> JSONSpawners = new List<JSONSpawn>();
+
+                    foreach (var item in parsedObject.Points)
                     {
-                        foreach (var obj in Regex.Split(item.Objects2, "OBJ="))
-                        {
-                            string[] spawnsplits = Regex.Split(obj, ":");
+                        // Save to model list NOTICE: Temp 
+                        List<PointEntry> pointEntries = new List<PointEntry>();
 
-                            var pointEntry = new PointEntry();
-                            pointEntry.Name = spawnsplits[0];
-                            pointEntry.MX = Int32.Parse(Regex.Match(spawnsplits[1], @"\d+").Value);
-                            pointEntry.SB = Int32.Parse(Regex.Match(spawnsplits[2], @"\d+").Value);
-                            pointEntry.RT = Int32.Parse(Regex.Match(spawnsplits[3], @"\d+").Value);
-                            pointEntry.TO = Int32.Parse(Regex.Match(spawnsplits[4], @"\d+").Value);
-                            pointEntry.KL = Int32.Parse(Regex.Match(spawnsplits[5], @"\d+").Value);
-                            pointEntry.RK = Int32.Parse(Regex.Match(spawnsplits[6], @"\d+").Value);
-                            pointEntry.CA = Int32.Parse(Regex.Match(spawnsplits[7], @"\d+").Value);
-                            pointEntry.DN = Int32.Parse(Regex.Match(spawnsplits[8], @"\d+").Value);
-                            pointEntry.DX = Int32.Parse(Regex.Match(spawnsplits[9], @"\d+").Value);
-                            pointEntry.SP = Int32.Parse(Regex.Match(spawnsplits[10], @"\d+").Value);
-                            pointEntry.PR = Int32.Parse(Regex.Match(spawnsplits[11], @"\d+").Value);
+                        if (item.Objects2 != "" && item.Objects2.Contains("OBJ="))
+                        {
+                            foreach (var obj in Regex.Split(item.Objects2, "OBJ="))
+                            {
+                                string[] spawnsplits = Regex.Split(obj, ":");
+
+                                var pointEntry = new PointEntry
+                                {
+                                    Name = spawnsplits[0],
+                                    MX = int.Parse(Regex.Match(spawnsplits[1], @"\d+").Value),
+                                    SB = int.Parse(Regex.Match(spawnsplits[2], @"\d+").Value),
+                                    RT = int.Parse(Regex.Match(spawnsplits[3], @"\d+").Value),
+                                    TO = int.Parse(Regex.Match(spawnsplits[4], @"\d+").Value),
+                                    KL = int.Parse(Regex.Match(spawnsplits[5], @"\d+").Value),
+                                    RK = int.Parse(Regex.Match(spawnsplits[6], @"\d+").Value),
+                                    CA = int.Parse(Regex.Match(spawnsplits[7], @"\d+").Value),
+                                    DN = int.Parse(Regex.Match(spawnsplits[8], @"\d+").Value),
+                                    DX = int.Parse(Regex.Match(spawnsplits[9], @"\d+").Value),
+                                    SP = int.Parse(Regex.Match(spawnsplits[10], @"\d+").Value),
+                                    PR = int.Parse(Regex.Match(spawnsplits[11], @"\d+").Value)
+                                };
+
+                                pointEntries.Add(pointEntry);
+                            }
+                        }
+                        else if (item.Objects2 != "")
+                        {
+                            string[] spawnsplits = Regex.Split(item.Objects2, ":");
+
+                            var pointEntry = new PointEntry
+                            {
+                                Name = spawnsplits[0],
+                                MX = int.Parse(Regex.Match(spawnsplits[1], @"\d+").Value),
+                                SB = int.Parse(Regex.Match(spawnsplits[2], @"\d+").Value),
+                                RT = int.Parse(Regex.Match(spawnsplits[3], @"\d+").Value),
+                                TO = int.Parse(Regex.Match(spawnsplits[4], @"\d+").Value),
+                                KL = int.Parse(Regex.Match(spawnsplits[5], @"\d+").Value),
+                                RK = int.Parse(Regex.Match(spawnsplits[6], @"\d+").Value),
+                                CA = int.Parse(Regex.Match(spawnsplits[7], @"\d+").Value),
+                                DN = int.Parse(Regex.Match(spawnsplits[8], @"\d+").Value),
+                                DX = int.Parse(Regex.Match(spawnsplits[9], @"\d+").Value),
+                                SP = int.Parse(Regex.Match(spawnsplits[10], @"\d+").Value),
+                                PR = int.Parse(Regex.Match(spawnsplits[11], @"\d+").Value)
+                            };
 
                             pointEntries.Add(pointEntry);
+                        }
 
+                        // Creates the output list needed for JSONSpawnElement
+                        List<JSONSpawnEntry> JSONSpawnElementEntries = new List<JSONSpawnEntry>();
+                        foreach (var spawnEntry in pointEntries)
+                        {
+                            JSONSpawnElementEntries.Add(
+                                new JSONSpawnEntry()
+                                {
+                                    maxCount = spawnEntry.MX,
+                                    name = spawnEntry.Name,
+                                    probability = 100
+                                });
+                        }
+
+                        JSONSpawn entry = new JSONSpawn()
+                        {
+                            type = "Spawner",
+                            location = new int[3]
+                                { int.Parse(item.CentreX), int.Parse(item.CentreY), int.Parse(item.CentreZ) },
+                            map = item.Map,
+                            count = int.Parse(item.MaxCount),
+                            entries = JSONSpawnElementEntries,
+                            homeRange = int.Parse(item.Range),
+                            walkingRange = int.Parse(item.Range),
+                            maxDelay = TimeSpan.FromMinutes(int.Parse(item.MaxDelay)).ToString(),
+                            minDelay = TimeSpan.FromMinutes(int.Parse(item.MinDelay)).ToString()
+                        };
+
+                        if (string.Equals(entry.map, facet, StringComparison.CurrentCultureIgnoreCase))
+                        {
+                            JSONSpawners.Add(entry);
                         }
                     }
-                    else if (item.Objects2 != "")
+                    
+                    var jsonString = JsonSerializer.Serialize(JSONSpawners, new JsonSerializerOptions
                     {
-                        string[] spawnsplits = Regex.Split(item.Objects2, ":");
+                        WriteIndented = true
+                    });
 
-                        var pointEntry = new PointEntry();
-                        pointEntry.Name = spawnsplits[0];
-                        pointEntry.MX = Int32.Parse(Regex.Match(spawnsplits[1], @"\d+").Value);
-                        pointEntry.SB = Int32.Parse(Regex.Match(spawnsplits[2], @"\d+").Value);
-                        pointEntry.RT = Int32.Parse(Regex.Match(spawnsplits[3], @"\d+").Value);
-                        pointEntry.TO = Int32.Parse(Regex.Match(spawnsplits[4], @"\d+").Value);
-                        pointEntry.KL = Int32.Parse(Regex.Match(spawnsplits[5], @"\d+").Value);
-                        pointEntry.RK = Int32.Parse(Regex.Match(spawnsplits[6], @"\d+").Value);
-                        pointEntry.CA = Int32.Parse(Regex.Match(spawnsplits[7], @"\d+").Value);
-                        pointEntry.DN = Int32.Parse(Regex.Match(spawnsplits[8], @"\d+").Value);
-                        pointEntry.DX = Int32.Parse(Regex.Match(spawnsplits[9], @"\d+").Value);
-                        pointEntry.SP = Int32.Parse(Regex.Match(spawnsplits[10], @"\d+").Value);
-                        pointEntry.PR = Int32.Parse(Regex.Match(spawnsplits[11], @"\d+").Value);
+                    var fileName = Path.GetFileName(targetPath);
 
-                        pointEntries.Add(pointEntry);
-                    }
-
-                    // Creates the output list needed for JSONSpawnElement
-                    List<JSONSpawnEntry> JSONSpawnElementEntries = new List<JSONSpawnEntry>();
-                    foreach (var spawnEntry in pointEntries)
-                    {
-
-                        JSONSpawnElementEntries.Add(
-                            new JSONSpawnEntry()
-                            {
-                                maxCount = spawnEntry.MX,
-                                name = spawnEntry.Name,
-                                probability = 100
-
-                            });
-                    }
-
-                    JSONSpawn entry = new JSONSpawn()
-                    {
-                        type = "Spawner",
-                        location = new int[3] { Int32.Parse(item.CentreX), Int32.Parse(item.CentreY), Int32.Parse(item.CentreZ) },
-                        map = item.Map,
-                        count = Int32.Parse(item.MaxCount),
-                        entries = JSONSpawnElementEntries,
-                        homeRange = Int32.Parse(item.Range),
-                        walkingRange = Int32.Parse(item.Range),
-                        maxDelay = TimeSpan.FromMinutes(Int32.Parse(item.MaxDelay)).ToString(),
-                        minDelay = TimeSpan.FromMinutes(Int32.Parse(item.MinDelay)).ToString()
-
-
-                    };
-
-                    if (entry.map.ToLower() == facet.ToLower())
-                    {
-                        JSONSpawners.Add(entry);
-                    }
-                                       
-
-
-
+                    File.WriteAllText(
+                        string.IsNullOrEmpty(fileName)
+                            ? Path.Combine(targetPath, $"{Path.GetFileNameWithoutExtension(currentFile)}.json")
+                            : targetPath, jsonString);
                 }
-
-                var jsonString = JsonSerializer.Serialize(JSONSpawners);
-                File.WriteAllText(targetpath, jsonString);
-
-
             }
         }
     }
